ab1ec902dedc696553715be77dfd3c3be16c45ac
Ian Rose <ianrosedev@gmail.com>
2016-04-26 01:13:37 -0400

    Finally fixed

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//Exit by sending SIGTERM to ->Node.js->Execute command 
 
 var express = require('express');
 var app     = express();
@@ -17,3 +16,16 @@ var handlebars = require('express-handlebars').create({
       return null;
     }
   }
+});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// View at localhost:8080
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Express started on http://localhost:' +
+             app.get('port'));
+});
