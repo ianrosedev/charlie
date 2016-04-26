0b12950ad123d598eb1945b0f2fba3e279b969ce
Ian Rose <ianrosedev@gmail.com>
2016-04-26 00:16:42 -0400

    Updates as I learn

--- app.js
+++ app.js
@@ -2,10 +2,36 @@
 
 var express = require('express');
 var app     = express();
-var path    = require('path');
 
-//Twitter API
+app.set('port', process.env.PORT || 8080);
+app.use(express.static(__dirname + '/public'));
 
+//set up handlebars view engine
+var handlebars = require('express-handlebars').create({
+  defaultLayout: 'main',
+  //sections helper
+  helpers: {
+  section: function(name, options) {
+      if(!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    }
+  }
+});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// View at localhost:8080
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Express started on http://localhost:' +
+             app.get('port'));
+});
+
+/*//Twitter API
 var twitter = require('twitter');
 var client = new twitter({
 	consumer_key: 'UUGfWBPmsOHM4AyXuhRpuPC1Z',
@@ -14,7 +40,7 @@ var client = new twitter({
 	access_token_secret: '2qYDyXUJaeCiUk4lVK44lpJxx6AnZ3S037aXJt8lybfN2'
 });
 
-client.stream('statuses/filter', {track: '@altonbrown'}, function(stream) {
+client.stream('statuses/filter', {track: '@SFGiants'}, function(stream) {
   stream.on('data', function(tweet) {
     console.log(tweet.user.name + ' --> ', tweet.text);
   });
@@ -22,13 +48,24 @@ client.stream('statuses/filter', {track: '@altonbrown'}, function(stream) {
   stream.on('error', function(error) {
     throw error;
   });
+}); */
+
+/*
+//Yelp API
+var Yelp = require('yelp');
+var yelp = new Yelp({
+  consumer_key: 'a1DR6z99oBjRl6lLboB-_A',
+  consumer_secret: 'o7XrLbHRmgHZpNL8IUdkWBVFHBU',
+  token: 'Oc13JBYgBwiXzf48mQ83jkNT8p3bA3YI',
+  token_secret: '1-tCgpafThuXNmQUuHFVvmBrVFI',
 });
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname + '/index.html'));
+// See http://www.yelp.com/developers/documentation/v2/search_api
+yelp.search({term: 'nachos', location: 'durham'})
+.then(function (data) {
+  console.log(data);
+})
+.catch(function (err) {
+  console.error(err);
 });
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(8080);
-console.log('\nRunning at Port 8080');
+*/
